64db88973f1a8356a8cdcd011ea3dfc57c21001c
maryamkhaann40-code <maryamkhaann40@gmail.com>
2026-08-05 19:28:49 +0500

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import CartProvider from "./context/CartContext";
+import MenuProvider from "./context/MenuContext";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Maryam Restaurant",
+  description: "Restaurant Website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <MenuProvider>
+          <CartProvider>
+            {children}
+          </CartProvider>
+        </MenuProvider>
+      </body>
+    </html>
+  );
+}
